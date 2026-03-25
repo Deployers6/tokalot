37800538339147d89015cddd59054ef773737c99
Tergel1116 <tergelbaasan4@gmail.com>
2026-03-25 18:06:15 +0800

    half of the lessons page added

---
+++ client/app/admin/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-screen h-15 bg-black text-white font-extrabold text-2xl flex items-center pl-5">
+      Tokalot
+    </div>
+  );
+};
+
+export default Header;
