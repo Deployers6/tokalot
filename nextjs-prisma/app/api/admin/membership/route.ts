a9406fe6b518e0eabcb5b8a046011782cf89f7ec
25LP5308 25LP5308 <telmuunotgonbileg@gmail.com>
2026-04-01 11:29:07 +0800

    wooow

--- nextjs-prisma/app/api/admin/membership/route.ts
+++ nextjs-prisma/app/api/admin/membership/route.ts
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { clerkId: string } } 
-) {
+
+export async function GET(req: NextRequest) {
   
-  const clerkId = req.headers.get("x-user-id") || params.clerkId;
+  const clerkId = req.headers.get("x-user-id");
 
   if (!clerkId || clerkId === "null") {
     return NextResponse.json({ error: "clerkId олдсонгүй" }, { status: 400 });
