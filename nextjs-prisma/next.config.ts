af4bd81e8c039d2cf442cb27e8b7d1ffd0810fd5
Tergel1116 <tergelbaasan4@gmail.com>
2026-04-06 11:55:18 +0800

    a

--- nextjs-prisma/next.config.ts
+++ nextjs-prisma/next.config.ts
@@ -4,29 +4,38 @@ const nextConfig: NextConfig = {
   env: {
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.unsplash.com",
+      },
+    ],
+  },
   async headers() {
     return [
       {
         source: "/api/:path*",
-
         headers: [
           {
             key: "Access-Control-Allow-Origin",
             value: "http://localhost:3000",
           },
-
           {
             key: "Access-Control-Allow-Methods",
             value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
           },
-
           {
             key: "Access-Control-Allow-Headers",
             value: "Content-Type, Authorization, x-admin-id, x-user-id",
           },
           {
             key: "Access-Control-Allow-Credentials",
-            value: "true", // <--- ЭНД НЭМЭЭРЭЙ
+            value: "true",
           },
         ],
       },
