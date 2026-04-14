47b35217dfb99d9cc33db22f664d8334e37b1766
Tergel1116 <tergelbaasan4@gmail.com>
2026-04-14 14:09:02 +0800

    cors fixed

--- nextjs-prisma/next.config.ts
+++ nextjs-prisma/next.config.ts
@@ -1,3 +1,56 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   env: {
+//     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "plus.unsplash.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "*.unsplash.com",
+//       },
+//     ],
+//   },
+//   async headers() {
+//     const allowedOrigin =
+//       process.env.NODE_ENV === "production"
+//         ? "https://tokalot-1d4s.vercel.app"
+//         : "http://localhost:3000";
+
+//     return [
+//       {
+//         source: "/api/:path*",
+//         headers: [
+//           {
+//             key: "Access-Control-Allow-Origin",
+//             value: allowedOrigin,
+//           },
+//           {
+//             key: "Access-Control-Allow-Methods",
+//             value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+//           },
+//           {
+//             key: "Access-Control-Allow-Headers",
+//             value:
+//               "Content-Type, Authorization, x-admin-id, x-user-id, x-user-email, x-user-name",
+//           },
+//           {
+//             key: "Access-Control-Allow-Credentials",
+//             value: "true",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -15,37 +68,6 @@ const nextConfig: NextConfig = {
         hostname: "*.unsplash.com",
       },
     ],
-  },
-  async headers() {
-    const allowedOrigin =
-      process.env.NODE_ENV === "production"
-        ? "https://tokalot-1d4s.vercel.app"
-        : "http://localhost:3000";
-
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: allowedOrigin,
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "Content-Type, Authorization, x-admin-id, x-user-id, x-user-email, x-user-name",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-        ],
-      },
-    ];
   },
 };
 
