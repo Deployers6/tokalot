fb6e1d13c23e4246f4378251d0f39ecc8a9b1b4c
25LP5308 25LP5308 <telmuunotgonbileg@gmail.com>
2026-04-16 11:49:22 +0800

    proxy

--- nextjs-prisma/proxy.ts
+++ nextjs-prisma/proxy.ts
@@ -57,7 +57,7 @@ export default clerkMiddleware(async (auth, req) => {
         "Access-Control-Max-Age": "86400",
       },
     });
-  }};
+  };
 
 
   if (isAdminRoute(req)) {
@@ -93,3 +93,4 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
   ],
+}
